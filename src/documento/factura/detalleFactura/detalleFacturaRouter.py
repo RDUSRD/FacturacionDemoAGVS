2cb547d1539292309b11ada55d7f7433339e7704
RDUSRD <urdaneta816@gmail.com>
2025-07-18 14:16:56 -0400

    Refactor: remove unused endpoints and related schemas from detalleFacturaRouter and detalleFacturaService

--- src/documento/factura/detalleFactura/detalleFacturaRouter.py
+++ src/documento/factura/detalleFactura/detalleFacturaRouter.py
@@ -4,12 +4,6 @@ from database import get_db
 from src.documento.factura.detalleFactura.detalleFacturaService import (
     get_detalle_factura_by_id,
     get_all_detalles_factura,
-    get_or_create_detalle_factura,
-    update_detalle_factura,
-)
-from src.documento.factura.detalleFactura.detalleFacturaSchema import (
-    DetalleFacturaSchema,
-    DetalleFacturaUpdateSchema,
 )
 
 router = APIRouter(prefix="/detalle_factura", tags=["DetalleFactura"])
@@ -23,27 +17,6 @@ def get_detalles_factura(db: Session = Depends(get_db)):
 @router.get("/{detalle_factura_id}")
 def get_detalle_factura(detalle_factura_id: int, db: Session = Depends(get_db)):
     detalle_factura = get_detalle_factura_by_id(db, detalle_factura_id)
-    if not detalle_factura:
-        raise HTTPException(status_code=404, detail="Detalle de factura no encontrado")
-    return detalle_factura
-
-
-@router.post("/create")
-def create_or_get_detalle_factura_endpoint(
-    detalle_factura_data: DetalleFacturaSchema, db: Session = Depends(get_db)
-):
-    return get_or_create_detalle_factura(db, detalle_factura_data)
-
-
-@router.put("/{detalle_factura_id}")
-def update_detalle_factura_endpoint(
-    detalle_factura_id: int,
-    detalle_factura_data: DetalleFacturaUpdateSchema,
-    db: Session = Depends(get_db),
-):
-    detalle_factura = update_detalle_factura(
-        db, detalle_factura_id, detalle_factura_data
-    )
     if not detalle_factura:
         raise HTTPException(status_code=404, detail="Detalle de factura no encontrado")
     return detalle_factura
--- src/documento/factura/detalleFactura/detalleFacturaService.py
+++ src/documento/factura/detalleFactura/detalleFacturaService.py
@@ -1,11 +1,5 @@
 from sqlalchemy.orm import Session
 from src.documento.factura.detalleFactura.detalleFacturaModel import DetalleFactura
-from src.documento.factura.detalleFactura.detalleFacturaSchema import (
-    DetalleFacturaSchema,
-    DetalleFacturaUpdateSchema,
-)
-from src.documento.factura.facturaService import get_factura_by_id
-from src.producto.productoService import get_producto_by_id
 
 
 def get_all_detalles_factura(db: Session):
@@ -16,66 +10,3 @@ def get_detalle_factura_by_id(db: Session, detalle_factura_id: int):
     return (
         db.query(DetalleFactura).filter(DetalleFactura.id == detalle_factura_id).first()
     )
-
-
-def get_or_create_detalle_factura(
-    db: Session, detalle_factura_data: DetalleFacturaSchema
-):
-    detalle_factura = (
-        db.query(DetalleFactura)
-        .filter(
-            DetalleFactura.factura_id == detalle_factura_data.factura_id,
-            DetalleFactura.producto_id == detalle_factura_data.producto_id,
-        )
-        .first()
-    )
-    if not detalle_factura:
-        # Verificamos si existe la factura
-        factura = get_factura_by_id(db, detalle_factura_data.factura_id)
-        if not factura:
-            return {
-                "error": "La factura_id no existe y es requerida para crear un detalle de factura."
-            }
-
-        producto = get_producto_by_id(db, detalle_factura_data.producto_id)
-        if not producto:
-            return {
-                "error": "La producto_id no existe y es requerida para crear un detalle de factura."
-            }
-
-        if detalle_factura_data.cantidad <= 0:
-            return {"error": "La cantidad debe ser mayor a 0."}
-
-        # calcular el total
-        total = detalle_factura_data.cantidad * producto.precio
-
-        detalle_factura = DetalleFactura(**detalle_factura_data.model_dump())
-        detalle_factura.total = total
-        db.add(detalle_factura)
-        db.commit()
-        db.refresh(detalle_factura)
-    return detalle_factura
-
-
-def update_detalle_factura(
-    db: Session,
-    detalle_factura_id: int,
-    detalle_factura_data: DetalleFacturaUpdateSchema,
-):
-    detalle_factura = (
-        db.query(DetalleFactura).filter(DetalleFactura.id == detalle_factura_id).first()
-    )
-    if detalle_factura:
-        # Calcular el total si la cantidad ha cambiado
-        if detalle_factura_data.cantidad is not None:
-            producto = get_producto_by_id(db, detalle_factura.producto_id)
-            if producto:
-                total = detalle_factura_data.cantidad * producto.precio
-                detalle_factura.total = total
-        for key, value in detalle_factura_data.model_dump(exclude_unset=True).items():
-            setattr(detalle_factura, key, value)
-        db.commit()
-        db.refresh(detalle_factura)
-        return detalle_factura
-    else:
-        return {"error": "El detalle de factura con el ID especificado no existe."}
